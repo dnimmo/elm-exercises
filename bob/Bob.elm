8ae4e397cf2bd8c3956ba9941896fb1b1931cf12
David Nimmo <dnimmo@gmail.com>
2017-01-22 18:50:21 +0000

    "questions" and "shouting" tests passing

--- bob/Bob.elm
+++ bob/Bob.elm
@@ -1,6 +1,13 @@
 module Bob exposing (..)
 
+import String
+
 
 hey : String -> String
 hey message =
-    "Whatever."
+    if (String.toUpper message) == message then
+        "Whoa, chill out!"
+    else if String.contains "?" message then
+        "Sure."
+    else
+        "Whatever."
