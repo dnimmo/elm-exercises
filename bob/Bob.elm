1dcb696e9b101c9af8206e937ea9051b229eb28e
David Nimmo <dnimmo@gmail.com>
2017-01-22 19:10:58 +0000

    readability improvements

--- bob/Bob.elm
+++ bob/Bob.elm
@@ -3,11 +3,27 @@ module Bob exposing (..)
 import String
 
 
+isQuestion : String -> Bool
+isQuestion message =
+    if String.endsWith "?" message then
+        True
+    else
+        False
+
+
+isUpperCase : String -> Bool
+isUpperCase message =
+    if (String.toUpper message) == message then
+        True
+    else
+        False
+
+
 hey : String -> String
 hey message =
-    if (String.toUpper message) == message then
+    if isUpperCase message then
         "Whoa, chill out!"
-    else if String.endsWith "?" message then
+    else if isQuestion message then
         "Sure."
     else
         "Whatever."
