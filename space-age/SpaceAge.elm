8a4bc099a0b9b68d65f51884c017820464a7646a
David Nimmo <dnimmo@gmail.com>
2017-03-02 22:54:58 +0000

    space-age tests passing

---
+++ space-age/SpaceAge.elm
@@ -0,0 +1,50 @@
+module SpaceAge exposing (..)
+
+
+type Planet
+    = Mercury
+    | Venus
+    | Earth
+    | Mars
+    | Jupiter
+    | Saturn
+    | Uranus
+    | Neptune
+
+
+getPlanetOffset : Planet -> Float
+getPlanetOffset planet =
+    case planet of
+        Mercury ->
+            0.2408467
+
+        Venus ->
+            0.61519726
+
+        Earth ->
+            1
+
+        Mars ->
+            1.8808158
+
+        Jupiter ->
+            11.862615
+
+        Saturn ->
+            29.447498
+
+        Uranus ->
+            84.016846
+
+        Neptune ->
+            164.79132
+
+
+secondsInYears : Float -> Planet -> Float
+secondsInYears seconds planet =
+    seconds / (31557600 * (getPlanetOffset planet))
+
+
+ageOn : Planet -> Float -> Float
+ageOn planet seconds =
+    secondsInYears seconds planet
